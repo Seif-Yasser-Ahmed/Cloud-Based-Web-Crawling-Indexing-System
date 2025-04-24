9b6731e5e0ad065d4bad6b2c28bef0d8bb5b8478
OSLMD <omarbayom3006@gmail.com>
2025-04-24 02:33:41 +0200

    Update aws_adapter.py

--- aws_adapter.py
+++ aws_adapter.py
@@ -1,57 +1,38 @@
 # aws_adapter.py
-import time
 import boto3
-from botocore.config import Config as BotoConfig
-from config import CRAWL_QUEUE_URL, INDEX_QUEUE_URL, S3_BUCKET, URL_TABLE, HEARTBEAT_TABLE
-
-boto_config = BotoConfig(retries={"max_attempts": 5, "mode": "standard"})
 
 class SqsQueue:
-    def __init__(self, url):
-        self.url = url
-        self.client = boto3.client("sqs", config=boto_config)
+    def __init__(self, identifier):
+        # Always tell boto your region:
+        self.client = boto3.client("sqs", region_name="eu-north-1")
+        if identifier.startswith("arn:aws:sqs"):
+            # ARN format: arn:aws:sqs:<region>:<account>:<queueName>
+            parts     = identifier.split(":")
+            queueName = parts[-1]
+            accountId = parts[4]
+            resp = self.client.get_queue_url(
+                QueueName=queueName,
+                QueueOwnerAWSAccountId=accountId
+            )
+            self.url = resp["QueueUrl"]
+        else:
+            self.url = identifier
 
-    def send(self, body, attrs=None):
-        self.client.send_message(QueueUrl=self.url, MessageBody=body, MessageAttributes=attrs or {})
-
-    def receive(self, max_messages=1, wait_time=5):
+    def receive(self):
         return self.client.receive_message(
             QueueUrl=self.url,
-            MaxNumberOfMessages=max_messages,
-            WaitTimeSeconds=wait_time,
-            MessageAttributeNames=["All"],
+            MaxNumberOfMessages=1,
+            WaitTimeSeconds=10
         ).get("Messages", [])
 
     def delete(self, receipt_handle):
-        self.client.delete_message(QueueUrl=self.url, ReceiptHandle=receipt_handle)
-
-
-class S3Client:
-    def __init__(self, bucket):
-        self.bucket = bucket
-        self.client = boto3.client("s3", config=boto_config)
-
-    def upload(self, key, data, content_type="text/html"):
-        self.client.put_object(Bucket=self.bucket, Key=key, Body=data, ContentType=content_type)
-
-
-class DynamoDBAdapter:
-    def __init__(self, table_name):
-        self.table = boto3.resource("dynamodb").Table(table_name)
-
-    def set_state(self, url, state, **attrs):
-        item = {"url": url, "state": state, "timestamp": int(time.time())}
-        item.update(attrs)
-        self.table.put_item(Item=item)
-
-    def get_stalled(self, timeout):
-        cutoff = int(time.time()) - timeout
-        resp = self.table.scan(
-            FilterExpression="state = :ip AND timestamp < :cutoff",
-            ExpressionAttributeValues={":ip": "IN_PROGRESS", ":cutoff": cutoff},
+        return self.client.delete_message(
+            QueueUrl=self.url,
+            ReceiptHandle=receipt_handle
         )
-        return [i["url"] for i in resp.get("Items", [])]
 
-    def log_heartbeat(self, worker_id):
-        hb_table = boto3.resource("dynamodb").Table(HEARTBEAT_TABLE)
-        hb_table.put_item(Item={"worker_id": worker_id, "last_seen": int(time.time())})
+    def send(self, body):
+        return self.client.send_message(
+            QueueUrl=self.url,
+            MessageBody=body
+        )
